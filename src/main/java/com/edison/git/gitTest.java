53ca59c00b6cc085fa481d7b1758298433b895cf
Edison <edisonwu@aliyun.com>
2021-09-14 15:07:19 +0800

    first commit

---
+++ src/main/java/com/edison/git/gitTest.java
@@ -0,0 +1,11 @@
+package com.edison.git;
+
+/**
+ * @author edisonwu
+ * @create 2021-09-14 15:03
+ */
+public class gitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello Git!");
+    }
+}
