f60eeb4bdbde267db67e54b40dff6d9c9a695436
Edison <edisonwu@aliyun.com>
2021-09-14 15:09:46 +0800

    second commit

--- src/main/java/com/edison/git/gitTest.java
+++ src/main/java/com/edison/git/gitTest.java
@@ -7,5 +7,6 @@ package com.edison.git;
 public class gitTest {
     public static void main(String[] args) {
         System.out.println("Hello Git!");
+        System.out.println("Hello Git 2!");
     }
 }
